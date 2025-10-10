f146bbd9497592d117621bce73b9a914289d5bfe
Rafaela Artico <rafameiartico@gmail.com>
2025-10-10 13:47:47 -0300

    Fase de projeto - DQL

--- BancoDeDados/ddl_DevConnect.sql
+++ BancoDeDados/ddl_DevConnect.sql
@@ -2,69 +2,76 @@
 
 CREATE DATABASE bd_DevConect
 
-USE bd_DevConect
 
---TABELA USUARIO
-CREATE TABLE tb_usuario(
-id						INT					IDENTITY (1,1)		PRIMARY KEY
-,nome_completo			NVARCHAR(255)		NOT NULL
-,nome_usuario			NVARCHAR(50)		UNIQUE
-,email					NVARCHAR(255)		UNIQUE				NOT NULL
-,senha					NVARCHAR(50)		NOT NULL
-,foto_perfil_usuario	NVARCHAR(150)		NULL
-);
+	USE bd_DevConect
 
-SELECT * FROM tb_usuario
+	--TABELA USUARIO
+	CREATE TABLE tb_usuario(
+	id_usuario				INT					IDENTITY (1,1)		PRIMARY KEY
+	,nome_completo			NVARCHAR(255)		NOT NULL
+	,nome_usuario			NVARCHAR(50)		NOT NULL			UNIQUE
+	,email					NVARCHAR(255)		NOT NULL			UNIQUE				
+	,senha					NVARCHAR(50)		NOT NULL
+	,foto_perfil_usuario	NVARCHAR(150)		NULL
+	);
 
-
---TABELA PUBLICAÇÃO
-CREATE TABLE tb_publicacao(
-id					INT				IDENTITY (1,1)	PRIMARY KEY
-,descricao			NVARCHAR(280)	NOT NULL
-,imagem_url			NVARCHAR(150)	NULL
-,data_publicacao	DATE			NOT NULL
-,id_usuario			INT				NOT NULL
-
-FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id)
-);
-
-SELECT * FROM tb_publicacao
+	SELECT * FROM tb_usuario
 
 
---TABELA COMENTARIO
-CREATE TABLE tb_comentario(
-id					INT				IDENTITY (1,1)	PRIMARY KEY
-,texto				NVARCHAR(1000)	NOT NULL
-,data_comentario	DATETIME		NOT NULL
-,id_publicacao      INT				NOT NULL
+	--TABELA PUBLICAÇÃO
+	CREATE TABLE tb_publicacao(
+	id_publicacao		INT				IDENTITY (1,1)	PRIMARY KEY
+	,descricao			NVARCHAR(280)	NOT NULL
+	,imagem_url			NVARCHAR(150)	NULL
+	,data_publicacao	DATE			NOT NULL
+	,id_usuario			INT				NOT NULL
 
-FOREIGN KEY (id_publicacao)	REFERENCES tb_publicacao(id)
-);
+	FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+	);
 
-SELECT * FROM tb_comentario
-
-
---TABELA CURTIDA
-CREATE TABLE tb_curtida(
-id			INT		IDENTITY (1,1)	PRIMARY KEY
-
-,id_usuario INT		NOT NULL
-FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id)
-
-,id_publicacao INT		NOT NULL
-FOREIGN KEY (id_publicacao) REFERENCES tb_publicacao(id)
-);
-
-SELECT * FROM tb_curtida
+	SELECT * FROM tb_publicacao
 
 
+	--TABELA COMENTARIO
+	CREATE TABLE tb_comentario(
+	id_comentario		INT				IDENTITY (1,1)	PRIMARY KEY
+	,texto				NVARCHAR(1000)	NOT NULL
+	,data_comentario	DATETIME2		NOT NULL
+	,id_publicacao      INT				NOT NULL
 
---TABELA SEGUIDOR - TABELA INTERMEDIÁRIA
-CREATE TABLE tb_seguidor(
-id_seguindo INT NOT NULL
-,id_seguidor INT NOT NULL
+	FOREIGN KEY (id_publicacao)	REFERENCES tb_publicacao(id_publicacao)
+	);
 
-PRIMARY KEY(id_seguindo, id_seguidor)
-);
+	ALTER TABLE tb_comentario
+	ALTER COLUMN data_comentario DATETIME2 NOT NULL;
 
-SELECT * FROM tb_seguidor
+
+	SELECT * FROM tb_comentario
+
+
+	--TABELA CURTIDA
+	CREATE TABLE tb_curtida(
+	id_curtida		INT		IDENTITY (1,1)	PRIMARY KEY
+
+	,id_usuario INT		NOT NULL
+	FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+
+	,id_publicacao INT		NOT NULL
+	FOREIGN KEY (id_publicacao) REFERENCES tb_publicacao(id_publicacao)
+	);
+
+	SELECT * FROM tb_curtida
+
+
+
+	--TABELA SEGUIDOR - TABELA INTERMEDIÁRIA
+	CREATE TABLE tb_seguidor(
+	id_seguindo INT NOT NULL
+	,id_seguidor INT NOT NULL
+
+	PRIMARY KEY(id_seguindo, id_seguidor)
+	FOREIGN KEY (id_seguidor) REFERENCES tb_usuario(id_usuario),
+	FOREIGN KEY (id_seguindo) REFERENCES tb_usuario(id_usuario)
+	);
+
+	SELECT * FROM tb_seguidor
--- BancoDeDados/dml DevConnect.sql
+++ BancoDeDados/dml DevConnect.sql
@@ -10,20 +10,28 @@ INSERT INTO tb_usuario(nome_completo, nome_usuario, email, senha, foto_perfil_us
 VALUES('Maria Eduarda dos Santos Cordeiro', 'Maduda', 'madudinha@gmail.com', 'maria Edu', 'www.com.DevConnect/mariaEduarda'),
 ('Anna Beatriz de Arenas Henkel', 'AnaBi', 'beatrizanna@gmail.com', 'Anna Bi', 'www.com.DevConnect/Anna-Beatriz');
 
-SELECT * FROM tb_usuario
+INSERT INTO tb_usuario(nome_completo, nome_usuario, email, senha, foto_perfil_usuario)
+VALUES
+('Anne Nicole de Lima Pereira', 'annex', 'Anne@gmail.com', 'Picanha!@#', 'www.com.DevConnect/AnneNicole'),
+('Ivan Aquino da Silva', 'Ivannn', 'Ivan@gmail.com', 'Zumbidospalmares', 'www.com.DevConnect/ivan'),
+('Kaua Fidalgo Droppa Garcia', 'k7.Fidalgo', 'fidalgo@gmail.com', 'hollownight', 'www.com.DevConnect/fidalgo');
 
+INSERT INTO tb_usuario(nome_completo, nome_usuario, email, senha, foto_perfil_usuario)
+VALUES('Luisa Meirelles Artico', 'Luli', 'luisa@gmail.com', 'luli123', 'www.com.DevConnect/luisa')
+
+SELECT * FROM tb_usuario
 
 --REGISTRO TABELA USUÁRIO
 INSERT INTO tb_publicacao(descricao, imagem_url, data_publicacao, id_usuario)
-VALUES('em um passeio com os meus amigos', 'www.com.DevConnect/passeio', '2025/10/03', 1);
+VALUES('em um passeio com os meus amigos', 'www.com.DevConnect/passeio', '2025/10/03', 1),
+('Praia', 'www.com.DevConnect/anaBI', '2025/11/19', 3),
+(':)', 'www.com.DevConnect/k7.Fidalgo', '2025/11/21', 6);
 
 SELECT * FROM tb_publicacao
 
---REGISTRO COMENTARIO
-INSERT INTO tb_comentario(texto, data_comentario, id_publicacao)
-VALUES('esse dia foi muito incrivel!', '01:38:56 2025/10/12', 1);
 
-SELECT * FROM tb_comentario
+SELECT * FROM tb_comentario;
+
 
 
 --REGISTRO CURTIDA
@@ -35,11 +43,22 @@ VALUES
 (2,1),
 (3,1);
 
-SELECT * FROM tb_curtida
+SELECT * FROM tb_curtida;
 
 
 --REGISTRO DA TABELA INTERMEDIARIA SEGUIDOR
 INSERT INTO tb_seguidor(id_seguindo, id_seguidor)
 VALUES(1,2);
+
+INSERT INTO tb_seguidor(id_seguindo, id_seguidor)
+VALUES
+(1,5),
+(2,6),
+(6,3),
+(5,3),
+(5,2),
+(4,1),
+(3,2),
+(1,3);
 
 SELECT * FROM tb_seguidor
